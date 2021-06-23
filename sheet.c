1c7de2022175e4d05907cbe3f421ddd2179c2dc8
EsZHU <2433149406@qq.com>
2021-06-23 18:38:43 +0800

    11.消除闪烁（1）

--- sheet.c
+++ sheet.c
@@ -46,39 +46,46 @@ void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, i
 	return;
 }
 
-void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0)
 {
 	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
 	unsigned char *buf, c, *vram = ctl->vram;
 	struct SHEET *sht;
 	/*如果refresh的范围超出了画面则修正*/
-	if (vx0 < 0) { vx0 = 0; }
-	if (vy0 < 0) { vy0 = 0; }
-	if (vx1 > ctl->xsize) { vx1 = ctl->xsize; }
-	if (vy1 > ctl->ysize) { vy1 = ctl->ysize; }
-	for (h = 0; h <= ctl->top; h++) {
-		sht = ctl->sheets[h];
-		buf = sht->buf;
-		/*使用vx0～vy1，逆算bx0～by1*/
-		bx0 = vx0 - sht->vx0;
-		by0 = vy0 - sht->vy0;
-		bx1 = vx1 - sht->vx0;
-		by1 = vy1 - sht->vy0;
-		if (bx0 < 0) { bx0 = 0; }
-		if (by0 < 0) { by0 = 0; }
-		if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
-		if (by1 > sht->bysize) { by1 = sht->bysize; }
-		for (by = by0; by < by1; by++) {
-			vy = sht->vy0 + by;
-			for (bx = bx0; bx < bx1; bx++) {
-				vx = sht->vx0 + bx;
-				c = buf[by * sht->bxsize + bx];
-				if (c != sht->col_inv) {
-					vram[vy * ctl->xsize + vx] = c;
-				}
-			}
-		}
-	}
+    if (vx0 < 0) {vx0 = 0;}
+    if (vy0 < 0) {vy0 = 0;}
+    if (vx1 > ctl->xsize) {vx1 = ctl->xsize;}
+    if (vy1 > ctl->ysize) {vy1 = ctl->ysize;}
+    for (h = h0; h <= ctl->top; h++) {
+        sht = ctl->sheets[h];
+        buf = sht->buf;
+        bx0 = vx0 - sht->vx0;
+        by0 = vy0 - sht->vy0;
+        bx1 = vx1 - sht->vx0;
+        by1 = vy1 - sht->vy0;
+        if (bx0 < 0) {
+            bx0 = 0;
+        }
+        if (by0 < 0) {
+            by0 = 0;
+        }
+        if (bx1 > sht->bxsize) {
+            bx1 = sht->bxsize;
+        }
+        if (by1 > sht->bysize) {
+            by1 = sht->bysize;
+        }
+        for (by = by0; by < by1; by++) {
+            vy = sht->vy0 + by;
+            for (bx = bx0; bx < bx1; bx++) {
+                vx = sht->vx0 + bx;
+                c = buf[by * sht->bxsize + bx];
+                if (c != sht->col_inv) {
+                    vram[vy * ctl->xsize + vx] = c;
+                }
+            }
+        }
+    }
 	return;
 }
 
@@ -110,8 +117,8 @@ void sheet_updown(struct SHEET *sht, int height)
 			}
 			ctl->top--; 
 		}
-		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
-	} else if (old < height) {	
+                sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height + 1);
+        } else if (old < height) {	
 		if (old >= 0) {
 			for (h = old; h < height; h++) {
 				ctl->sheets[h] = ctl->sheets[h + 1];
@@ -126,15 +133,15 @@ void sheet_updown(struct SHEET *sht, int height)
 			ctl->sheets[height] = sht;
 			ctl->top++; 
 		}
-		sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
-	}
+                sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, 0);
+    }
 	return;
 }
 
 void sheet_refresh(struct SHEET* sht, int bx0, int by0, int bx1, int by1)
 {
 	if (sht->height >= 0) { /*如果正在显示，则按新图层的信息进行刷新*/
-            sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
+            sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1, sht->height);
         }
 	return;
 }
@@ -145,8 +152,8 @@ void sheet_slide(struct SHEET* sht, int vx0, int vy0)
 	sht->vx0 = vx0;
 	sht->vy0 = vy0;
 	if (sht->height >= 0) {/*如果正在显示，则按新图层的信息进行刷新*/
-        sheet_refreshsub(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
-        sheet_refreshsub(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
+        sheet_refreshsub(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize, 0);
+        sheet_refreshsub(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize, sht->height);
     }
         return;
 }
